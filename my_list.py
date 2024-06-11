02b4a008feeda4d227f0da3feafa0600a19eb628
LetsMakeTheTryWork <rocckyrocky001@gmail.com>
2024-06-11 21:13:14 +0300

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,35 @@
+#Create an empty list called my_list.
+my_list=[]
+
+#Append the following elements to my_list: 10, 20, 30, 40
+my_list=[]
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+
+# my_list.insert(1, 15)
+print(my_list)
+
+#Extend my_list with another list: [50, 60, 70].
+my_list.extend([50, 60, 70])
+print(my_list)
+
+#Remove the last element from my_list.
+my_list = [10, 20, 30, 40, 50, 60, 70]
+my_list.pop()
+print(my_list)
+
+#Sort my_list in ascending order.
+my_list = [10, 20, 30, 40, 50, 60, 70]
+my_list.sort()
+print(my_list)
+
+#Find and print the index of the value 30 in my_list.
+my_list = [10, 20, 30, 40, 50, 60, 70]
+index_30 = my_list.index(30)
+print("Index of 30:", index_30)
+
+print(my_list)
